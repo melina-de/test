3af31de8cb6a1f4e69d74613e0d88ea2a0529fca
setayesh <nejadmohammad.setayesh@gmail.com>
2024-12-03 13:55:16 +0330

    add the table1 to the main window

--- main.c
+++ main.c
@@ -3,21 +3,23 @@
 
 int main(void) {
     InitWindow(0, 0, "main screen");
-    //InitWindow(400, 400, "not main screen");
+
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
-    Texture2D duck= LoadTexture("D://FUM//projects//elysian//duck.png");
+    Texture2D duck = LoadTexture("D://FUM//projects//elysian//duck.png");
+    Texture2D table1 = LoadTexture("D://FUM//projects//test//table1.jpg");
+
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         BeginDrawing();
 //salalsjlakdlkajshfljkqwglefigqlewguf
 
-        ClearBackground(GRAY);
+        ClearBackground(BLACK);
 
-        DrawText("Elysian!", 850, 500, 80, BLACK);
+        //DrawText("Elysian!", 850, 500, 80, BLACK);
         //DrawTexture(duck,100,100,WHITE);
-
+        DrawTexture(table1,450,50,WHITE);
         EndDrawing();
 
     }
